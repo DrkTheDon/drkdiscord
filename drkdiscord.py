6515cf4dca86b998a920a158220e82d9b104fed2
Darkest Surface <74876947+DaRkSurface@users.noreply.github.com>
2022-01-09 10:24:33 +0100

    Update drkdiscord.py

--- drkdiscord.py
+++ drkdiscord.py
@@ -11,24 +11,24 @@
 import time
 import pyfade
 import random
-from colorama import Fore
+from colorama import Fore, Style, init
 import os
+import requests
 
 # Global Variables
 drk = 14
 
 
 # Defines
-
 def banner():
   print(pyfade.Fade.Horizontal(pyfade.Colors.green_to_red, """
-  V. 1.0 Beta (January 9th 2022)
-      ██████╗ ██████╗ ██╗  ██╗    ██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗ 
-      ██╔══██╗██╔══██╗██║ ██╔╝    ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗
-      ██║  ██║██████╔╝█████╔╝     ██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║
-      ██║  ██║██╔══██╗██╔═██╗     ██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║
-      ██████╔╝██║  ██║██║  ██╗    ██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝
-      ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝    ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝                                  
+   V. 1.0 Beta (January 9th 2022)
+  ██████╗ ██████╗ ██╗  ██╗    ██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗ 
+  ██╔══██╗██╔══██╗██║ ██╔╝    ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗
+  ██║  ██║██████╔╝█████╔╝     ██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║
+  ██║  ██║██╔══██╗██╔═██╗     ██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║
+  ██████╔╝██║  ██║██║  ██╗    ██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝
+  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝    ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝                                  
                                                                           """))
   print(f'''{Fore.LIGHTWHITE_EX}                                [ Made by {Fore.YELLOW}drk#1337 {Fore.LIGHTWHITE_EX}]
   {Fore.LIGHTWHITE_EX}
@@ -48,7 +48,7 @@ def options():
   [1] Token Generator
   [2] Token Checker (Under Development)
   [3] Nitro Gift Generator
-  [4] Nitro Gift Checker (Under Development)
+  [4] Nitro Gift Checker (Not Stable)
   [5] Coming Soon 
 
   [6] Credits
@@ -68,9 +68,8 @@ def options():
     clearcmd()
     nitrogenerator()
   elif USER_OPTION == "4":
-    underdev()
-    back()
-    options()
+    clearcmd()
+    nitrocheck()
   elif USER_OPTION == "5":
     underdev()
     back()
@@ -86,6 +85,10 @@ def options():
   elif USER_OPTION == "8":
     clearcmd()
     quit()
+  else:
+    print(f"{Fore.RED}[-]{Fore.LIGHTWHITE_EX} Did not reckognize your input {Fore.YELLOW}{USER_OPTION}{Fore.LIGHTWHITE_EX}. Please enter one of the numbers above.")
+    back()
+    options()
     
 
 
@@ -101,7 +104,7 @@ def generatetoken():
   drk = "king"
   length = 51
   print(pyfade.Fade.Horizontal(pyfade.Colors.blue_to_cyan, """
-  DRK DISCORD
+   DRK DISCORD
   ████████╗ ██████╗ ██╗  ██╗███████╗███╗   ██╗     ██████╗ ███████╗███╗   ██╗
   ╚══██╔══╝██╔═══██╗██║ ██╔╝██╔════╝████╗  ██║    ██╔════╝ ██╔════╝████╗  ██║
      ██║   ██║   ██║█████╔╝ █████╗  ██╔██╗ ██║    ██║  ███╗█████╗  ██╔██╗ ██║
@@ -110,11 +113,11 @@ def generatetoken():
      ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝
                                                                            
                                                                           """))
-  print(f"""{Fore.LIGHTWHITE_EX} [ Made by {Fore.YELLOW}drk#1337 {Fore.LIGHTWHITE_EX}] WITH: {Fore.GREEN} DRK DISCORD PROJECT
+  print(f"""{Fore.LIGHTWHITE_EX} [ Made by {Fore.YELLOW}drk#1337 {Fore.LIGHTWHITE_EX}]{Fore.GREEN} DRK DISCORD PROJECT
   {Fore.LIGHTWHITE_EX}
   """)
   print("[*] Setting Up...\n")
-  time.sleep(1.5)
+  time.sleep(1)
 
   howmany = input(f"{Fore.BLUE}[?] How many tokens do you want to generate: ")
 
@@ -128,18 +131,16 @@ def generatetoken():
           file.write(result)
           file.write("\n")
 
-      file.close()
       print(f"\n{Fore.YELLOW}[*] Generating... \n")
       time.sleep(2)
       print(f"{Fore.GREEN}[+] Generated {Fore.RED} {howmany} {Fore.GREEN} Discord token(s). Saved to ./generated/token/tokens.txt ")
+      file.close()
       back()
       options()
 def nitrogenerator():
 
   randomgift = "https://discord.gift/N2td3PXwMdFCRgyj" # Just an expired link to watch.
   characters = "abcdefghijklmnopqrstuwyxzABCDEFGHIJKLMNOPQRSTUWYXZ1234567890"
-  length = 16
-  drk = "king"
   
   print(pyfade.Fade.Horizontal(pyfade.Colors.blue_to_cyan, """
    DRK DISCORD
@@ -151,26 +152,85 @@ def nitrogenerator():
   ╚═╝  ╚═══╝╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝      ╚═════╝ ╚══════╝╚═╝  ╚═══╝
                                                                       
                                                                           """))
-  print(f"""{Fore.LIGHTWHITE_EX} [ Made by {Fore.YELLOW}drk#1337 {Fore.LIGHTWHITE_EX}] WITH: {Fore.GREEN} DRK DISCORD PROJECT
+  print(f"""{Fore.LIGHTWHITE_EX} [ Made by {Fore.YELLOW}drk#1337 {Fore.LIGHTWHITE_EX}]{Fore.GREEN} DRK DISCORD PROJECT
   {Fore.LIGHTWHITE_EX}
   """)
   print("[*] Setting Up...\n")
-  time.sleep(1.5)
+  time.sleep(1)
 
-  howmany = input(f"{Fore.YELLOW}[?]{Fore.LIGHTWHITE_EX} How many tokens do you want to generate?: ")
+  howmany = input(f"{Fore.BLUE}[?] How many tokens do you want to generate?: ")
 
   with open("./generated/nitro/nitro.txt", "w+") as file:
       for i in range(int(howmany)):
-          genrandom = ''.join(random.choice(characters) for i in range(length))
+          genrandom = ''.join(random.choice(characters) for i in range(16))
 
           result = f"https://discord.gift/{genrandom}" 
           file.write(result)
           file.write("\n")
       print(f"\n{Fore.YELLOW}[*] Generating... \n")
       time.sleep(2)
-      print(f"{Fore.GREEN}[+] Generated {Fore.RED} {howmany} {Fore.GREEN} Discord token(s). Saved to ./generated/nitro/nitro.txt ")
+      print(f"{Fore.GREEN}[+] Generated {Fore.RED}{howmany}{Fore.GREEN} Discord token(s). Saved to ./generated/nitro/nitro.txt ")
 
-      file.close()
+  back()
+  file.close()
+  options()
+
+def nitrocheck():
+  init()
+
+  print(pyfade.Fade.Horizontal(pyfade.Colors.blue_to_cyan, """
+   DRK DISCORD
+  ███╗   ██╗██╗████████╗██████╗  ██████╗      ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗
+  ████╗  ██║██║╚══██╔══╝██╔══██╗██╔═══██╗    ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝
+  ██╔██╗ ██║██║   ██║   ██████╔╝██║   ██║    ██║     ███████║█████╗  ██║     █████╔╝ 
+  ██║╚██╗██║██║   ██║   ██╔══██╗██║   ██║    ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ 
+  ██║ ╚████║██║   ██║   ██║  ██║╚██████╔╝    ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗
+  ╚═╝  ╚═══╝╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝      ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝
+                                                                                    
+                                                                      
+                                                                          """))
+  print(f"""{Fore.LIGHTWHITE_EX} [ Made by {Fore.YELLOW}drk#1337 {Fore.LIGHTWHITE_EX}]{Fore.GREEN} DRK DISCORD PROJECT
+  {Fore.LIGHTWHITE_EX}
+  """)
+  print("[*] Setting Up...\n")
+  time.sleep(1)
+  def checkNitro(code):
+    
+    URL = f"https://discordapp.com/api/v9/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true"
+    response = requests.get(URL)
+    if response.status_code == 200:
+        return True
+    else: 
+        return False
+ 
+  
+  VALID_CODES_FILE   = "./generated/nitro/valid_nitro_codes.txt"
+  INVALID_CODES_FILE = "./generated/nitro/invalid_nitro_codes.txt"
+  CODES_FILE         = "./generated/nitro/nitro.txt"
+  if not os.path.exists(CODES_FILE):
+    print("No file with nitro was found, Go Generate some bro..")
+    back()
+    options()
+  validTokens_file   = open(VALID_CODES_FILE, "w+")
+  invalidTokens_file = open(INVALID_CODES_FILE, "w+")
+  
+  with open(CODES_FILE , "r+") as codes_file:
+      content = codes_file.readlines()
+      codes = list(filter(None, list(map(lambda x: str.replace(x, "\n", ""), content))))
+  
+      for code in codes:
+          status = checkNitro(code)
+          if status is True:
+              print(f'{Fore.GREEN}Valid {Fore.LIGHTWHITE_EX}| {code}', Fore.WHITE + Style.NORMAL)
+              validTokens_file.write(code + "\n")
+          else:
+              print(f'{Fore.RED}Invalid {Fore.LIGHTWHITE_EX}| {code}', Fore.WHITE + Style.NORMAL)
+              invalidTokens_file.write(code + "\n")
+
+      codes_file.flush()
+      os.fsync(codes_file.fileno())
+      codes_file.close()
+  print(f"{Fore.GREEN}[+]{Fore.LIGHTWHITE_EX} Script Done!")
   back()
   options()
 
